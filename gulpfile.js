1dc5704ddb5569614baf8e2fa3adf4787006101f
song <song@mc2.io>
2015-06-06 10:19:39 +0800

    finish basic layout

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var path = require('path'),
+    gulp = require('gulp'),
+    connect = require('gulp-connect'),
+    less = require('gulp-less');
+
+var compile = function(file) {
+    gulp.src(file)
+        .pipe(less().on('error', function(err) {
+            console.error(err.stack);
+        }))
+        .pipe(gulp.dest('.'))
+        .pipe(connect.reload());
+};
+
+gulp.task('default', function() {
+    gulp.watch(['*.less'], function(data) {
+        console.info(data.type + ': ' + data.path);
+        compile(data.path);
+    });
+});
