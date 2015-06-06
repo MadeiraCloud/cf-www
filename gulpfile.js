a957b4fdff48a86a82fe960d76cdf926d3e891e2
imeoer <imeoer@gmail.com>
2015-06-06 14:29:03 +0800

    compress css file

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,14 @@ var path = require('path'),
     gulp = require('gulp'),
     connect = require('gulp-connect'),
     less = require('gulp-less');
+    miniCSS = require('gulp-minify-css');
 
 var compile = function(file) {
     gulp.src(file)
         .pipe(less().on('error', function(err) {
             console.error(err.stack);
         }))
+        .pipe(miniCSS())
         .pipe(gulp.dest('.'))
         .pipe(connect.reload());
 };
@@ -17,4 +19,5 @@ gulp.task('default', function() {
         console.info(data.type + ': ' + data.path);
         compile(data.path);
     });
+    compile('*.less');
 });
